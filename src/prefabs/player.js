58ff94a17c3bac8401d368770ec1af48230001c8
Nick Marigo <nmarigo@ucsc.edu>
2026-02-04 09:38:53 -0800

    Change the player FSM so they can jump and slide. Also changed the movement so the player slowly drifts backwards to match the speed of the background

--- src/prefabs/player.js
+++ src/prefabs/player.js
@@ -8,18 +8,116 @@ class Player extends Phaser.Physics.Arcade.Sprite {
         this.body.setCollideWorldBounds(true);
 
         this.direction = direction;
-        this.playerVelocity = 200;
         
-        scene.playerFSM = new StateMachine('idle', {
-            idle: new IdleState(),
-            move: new MoveState(),
+        /*this.baseRunSpeed = 100;
+        this.speedAdjust = 120;
+        this.maxRunSpeed = 380;
+        this.minRunSpeed = -160;*/
+
+        this.scrollSpeed = 100;
+        this.playerMoveVelocity = 300;
+
+        this.jumpSpeed = 450;
+        this.isSliding = false;
+        
+        scene.playerFSM = new StateMachine('run', {
+            run: new RunState(),
+            jump: new JumpState(),
+            slide: new SlideState(),
         }, [scene, this]);
+    }
+
+    // Helper function to apply "endless runner" velocity every frame
+    applyRunVelocity(scene) {
+        
+        const { A, D } = scene.keys;
+
+        let vx = 0;
+
+        if(D.isDown) vx += this.playerMoveVelocity; 
+        if(A.isDown) vx -= this.playerMoveVelocity;
+
+        this.setVelocityX(vx - this.scrollSpeed);
+        
     }
 
 }
 
+class RunState extends State {
+
+    enter(scene, player) {
+        player.isSliding = false;
+        // restore size/offset if changed from slide
+        /*player.body.setSize(player.width, player.height);
+        player.body.setOffset(0, 0);
+        player.anims.play('run-' + player.direction, true);*/
+    }
+
+    execute(scene, player) {
+
+        const { W, S } = scene.keys;
+
+        player.applyRunVelocity(scene);
+
+        if(Phaser.Input.Keyboard.JustDown(W) && player.body.blocked.down) {
+            player.setVelocityY(-player.jumpSpeed);
+            this.stateMachine.transition('jump');
+            return;
+        }
+
+        if(S.isDown && player.body.blocked.down) {
+            this.stateMachine.transition('slide');
+            return;
+        }
+
+    }
+}
+
+class JumpState extends State {
+    execute(scene, player) {
+
+        const { S } = scene.keys;
+
+        player.applyRunVelocity(scene);
+
+        if(player.body.blocked.down) {
+            if (S.isDown) {
+                 this.stateMachine.transition('slide');
+            } else {
+                this.stateMachine.transition('run');
+            }
+        }
+    }
+}
+
+class SlideState extends State {
+    enter(scene, player) {
+        player.isSliding = true;
+
+        // adjust size/offset for slide
+        /*player.body.setSize();
+        player.body.setOffset();
+        player.anims.play('slide-' + player.direction, true);*/
+    }
+
+    execute(scene, player) {
+
+        const { S } = scene.keys;
+
+        player.applyRunVelocity(scene);
+
+        if(!S.isDown) {
+            if (player.body.blocked.down) {
+                this.stateMachine.transition('run');
+            } else {
+                this.stateMachine.transition('jump');
+            }   
+        }
+    }
+}
+
 // Player States
-class IdleState extends State { 
+/*class IdleState extends State { 
     enter(scene, player) {
         player.setVelocity(0);
         //player.anims.play('idle-' + player.direction, true);
@@ -62,4 +160,4 @@ class MoveState extends State {
     }
 
 
-}
+}*/
--- src/scenes/play.js
+++ src/scenes/play.js
@@ -18,19 +18,37 @@ class Play extends Phaser.Scene{
     create(){
 
         this.background = this.add.tileSprite(0, 0, width, height, 'background').setOrigin(0, 0);
-        this.platforms = this.physics.add.staticGroup();
-        this.platforms.create(width /2, height -100, 'platform').setOrigin(0.5, 0.5);;
-
-        this.player = new Player(this, 100, 450, 'player', 0, 'right');
-
-        this.physics.add.collider(this.player, this.platforms);
+        /*this.platforms = this.physics.add.group({ immovable: true, allowGravity: false });
+        const p = this.platforms.create(width /2, height -100, 'platform').setOrigin(0.5, 0.5);
+        p.setImmovable(true);
+        p.body.allowGravity = false;*/
 
         this.keys = this.input.keyboard.addKeys({
-            JUMP: Phaser.Input.Keyboard.KeyCodes.W,
-            DOWN: Phaser.Input.Keyboard.KeyCodes.S,
-            A: Phaser.Input.Keyboard.KeyCodes.A,
-            D: Phaser.Input.Keyboard.KeyCodes.D,
+            W: Phaser.Input.Keyboard.KeyCodes.W,     // JUMP
+            S: Phaser.Input.Keyboard.KeyCodes.S,     // SLIDE
+            A: Phaser.Input.Keyboard.KeyCodes.A,     // LEFT
+            D: Phaser.Input.Keyboard.KeyCodes.D,     // RIGHT
         });
+
+        this.scrollSpeed = 100;
+
+        this.platforms = this.physics.add.group({ immovable: true, allowGravity: false });
+
+        const platformWidth = 900;
+        const y = height - 100;
+
+        const count = Math.ceil(width / platformWidth) + 2;
+
+        for(let i = 0; i < count; i++) {
+            const p = this.platforms.create(i * platformWidth, y, 'platform').setOrigin(0, 0.5);
+            p.setImmovable(true);
+            p.body.allowGravity = false;
+            p.body.moves = false;
+        }
+
+        this.player = new Player(this, width / 2, 650, 'player', 0, 'right');
+
+        this.physics.add.collider(this.player, this.platforms);
 
         this.input.keyboard.on('keydown-G', () => {
             this.physics.world.drawDebug = this.physics.world.drawDebug ? false : true;
@@ -39,11 +57,37 @@ class Play extends Phaser.Scene{
 
     }
 
-    update(){
+    update(time, delta){
 
-        this.background.tilePositionX += 2;
+        const dt = delta / 1000;
 
         this.playerFSM.step();
+
+        this.background.tilePositionX += this.player.scrollSpeed * dt;
+
+        // Move platforms to create scrolling effect
+        const platforms = this.platforms.getChildren();
+        for (const p of platforms) {
+            p.x -= this.scrollSpeed * dt;
+        }
+
+        let rightMostX = -Infinity;
+
+        for(const p of platforms) rightMostX = Math.max(rightMostX, p.x);
+
+        const platformWidth = 900;
+        
+        for(const p of platforms) {
+            if(p.x + platformWidth < 0) {
+                p.x = rightMostX + platformWidth;
+                rightMostX = p.x;
+            }
+        }
+
+        if(this.player.x < 0) {
+            console.log("Game Over");
+
+        }
 
     }
 
