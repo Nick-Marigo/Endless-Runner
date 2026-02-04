09909a4052e9bbc144b527278897ccd77df125a4
Nick Marigo <nmarigo@ucsc.edu>
2026-02-04 15:46:42 -0800

    Got jumping working

--- src/prefabs/player.js
+++ src/prefabs/player.js
@@ -33,7 +33,7 @@ class Player extends Phaser.Physics.Arcade.Sprite {
         //console.log(`Flow Direction: ${flowDirection}`);
 
         // Calculate player speed (D = Forward, A = Backward)
-        let moveMagnitude = 0;
+        let moveMagnitude = -this.scrollSpeed;
         if(D.isDown) moveMagnitude += this.playerMoveVelocity; 
         if(A.isDown) moveMagnitude -= (this.playerMoveVelocity / 3);
 
@@ -43,6 +43,18 @@ class Player extends Phaser.Physics.Arcade.Sprite {
             this.setVelocityY(flowDirection.y * moveMagnitude);
         }
         
+    }
+
+    applyJump() {
+        const jumpVelocity = this.jumpSpeed;
+        const gravityVector = gravityDir[currentGravity];
+
+        // Jump in opposite direction of gravity
+        if(gravityVector.y !== 0) {
+            this.setVelocityY(gravityVector.y * - jumpVelocity);
+        } else if (gravityVector.x !== 0) {
+            this.setVelocityX(gravityVector.x * - jumpVelocity);
+        }
     }
 
     // Helper function to determine if player is grounded in any direction
@@ -75,13 +87,13 @@ class RunState extends State {
 
         player.applyRunVelocity(scene);
 
-        if(Phaser.Input.Keyboard.JustDown(W) && player.body.blocked.down) {
-            player.setVelocityY(-player.jumpSpeed);
+        if(Phaser.Input.Keyboard.JustDown(W) && player.isGrounded()) {
+            player.applyJump();
             this.stateMachine.transition('jump');
             return;
         }
 
-        if(S.isDown && player.body.blocked.down) {
+        if(S.isDown && player.isGrounded()) {
             this.stateMachine.transition('slide');
             return;
         }
@@ -96,7 +108,7 @@ class JumpState extends State {
 
         player.applyRunVelocity(scene);
 
-        if(player.body.blocked.down) {
+        if(player.isGrounded()) {
             if (S.isDown) {
                  this.stateMachine.transition('slide');
             } else {
@@ -123,7 +135,7 @@ class SlideState extends State {
         player.applyRunVelocity(scene);
 
         if(!S.isDown) {
-            if (player.body.blocked.down) {
+            if (player.isGrounded()) {
                 this.stateMachine.transition('run');
             } else {
                 this.stateMachine.transition('jump');
